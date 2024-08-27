b293162c54d88a9370f840b341df250f5e704c68
Ahtasham-ul-haq <ahtasham1802@gmail.com>
2024-08-27 05:27:51 +0500

    implement Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,16 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Highlights from "./components/Highlights"
 
-import {
-  GsapFrom,
-  GsapFromTo,
-  GsapScrollTrigger,
-  GsapStagger,
-  GsapText,
-  GsapTimeline,
-  GsapTo,
-  Home,
-} from "./pages";
+const App = () =>  {
 
-const App = () => {
   return (
-    <div className="bg-black min-h-screen w-full">
-      <Router>
-        <Routes>
-          <Route path="/gsapto" element={<GsapTo />} />
-          <Route path="/gsapfrom" element={<GsapFrom />} />
-          <Route path="/gsapfromto" element={<GsapFromTo />} />
-          <Route path="/gsaptimeline" element={<GsapTimeline />} />
-          <Route path="/gsapstagger" element={<GsapStagger />} />
-          <Route path="/gsapscrolltrigger" element={<GsapScrollTrigger />} />
-          <Route path="/gsaptext" element={<GsapText />} />
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
-  );
-};
+    <main>
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
 
-export default App;
+export default App
